eed63f569ed16ebec968cabdb914ca7ae05c9938
Blanca Civieta Bermejo <blancivi@gmail.com>
2025-07-05 14:08:32 +0200

    restriccion csp comentada

--- switkor-frontend/next.config.ts
+++ switkor-frontend/next.config.ts
@@ -5,3 +5,48 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+//restricicones csp para proyecto en produccoón real
+/*const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval';
+      style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+      img-src 'self' data: blob: https:;
+      font-src 'self' fonts.gstatic.com;
+      connect-src 'self' https://api.blancacivieta.es http://localhost:3001;
+    `.replace(/\n/g, ""),
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
+ */
